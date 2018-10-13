fca48bf19adb7bea2df4c3982ec1075a9529c8ae
Megan McClure <meganemcclure@gmail.com>
2018-10-13 17:03:14 -0400

    Update Date Class

---
+++ src/Date.java
@@ -0,0 +1,52 @@
+
+public class Date {
+	int year;
+	int month;
+	int day;
+	
+	/*
+	 * returns int year
+	 */
+	public int getYear() {
+		return year;
+	} // end getYear method
+	
+	/*
+	 * returns int month
+	 */
+	public int getMonth() {
+		return month;
+	} // end getMonth method
+	
+	/* 
+	 * returns int day
+	 */
+	public int getDay() {
+		return day;
+	} // end getDay method
+	
+	/*
+	 * validates that the year is within range
+	 * if the year is valid, sets year attribute
+	 */
+	public void setYear(int year) {
+		//TODO
+	} // end setYear method
+	
+	/*
+	 * validates that the month is within range
+	 * if the month is valid, sets month attribute
+	 */
+	public void setMonth(int month) {
+		//TODO
+	} // end setMonth method
+	
+	/*
+	 * validates that the day is within range
+	 * if the day is valid, sets day attribute
+	 */
+	public void setDay(int day) {
+		//TODO
+	} // end setDay method
+
+} // end Date class

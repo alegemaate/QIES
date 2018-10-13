d119d4f824658a2ec505cb0fb542f57aa1909d08
Irena <dunjicirena@gmail.com>
2018-10-13 17:29:34 -0400

    Update Services and ServiceCommands classes

---
+++ src/ServiceCommands.java
@@ -0,0 +1,23 @@
+
+public class ServiceCommands {
+
+	/*
+	 * Prompts user for new service number
+	 * Ensures valid service number
+	 * Ensures unique service number
+	 * Calls Services.add with the unique service number
+	 */
+	public static void createService() {
+		// TODO
+	} // end createService method
+	
+	/*
+	 * Prompts user for existing service number
+	 * Ensures existing service number
+     * Calls Services.remove with the unique service number
+	 */
+	public static void deleteService() {
+		// TODO
+	} // end deleteService method
+	
+} // end ServiceCommands class
---
+++ src/Services.java
@@ -0,0 +1,26 @@
+
+public class Services {
+	ArrayList<Services> serviceList = new ArrayList<Services>();
+	
+	/*
+	 * Reads the input file and populates the list with valid services
+	 */
+	public void readServices() {
+		// TODO
+	} // end readServices method
+	
+	/*
+	 * Adds service to the list of Services
+	 */
+	public void add(int serviceNumber) {
+		// TODO
+	} // end add method
+	
+	/*
+	 * Removes a service from the list of services
+	 */
+	public void remove(int serviceNumber) {
+		// TODO
+	} // end remove method
+
+} // end Services class

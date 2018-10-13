adbf0c40e0fd92a6554cf041198d63bb61afa41c
Allan Legemaate <alegemaate@gmail.com>
2018-10-13 18:41:19 -0400

    Validator for transaction directory and valid service file

--- src/QIESBase.java
+++ src/QIESBase.java
@@ -4,6 +4,11 @@
  * Spice Tests
  * 13/10/2018
  */
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+
 public class QIESBase {
 	// Input handler
 	public static InputHandler input = new InputHandler();
@@ -11,12 +16,27 @@ public class QIESBase {
 	public static void main(String [] args) {
 		// Ensure we are receiving a input file and a directory for output
 		if (args.length != 3 && false) {
-			System.out.println("You must provide a valid input file and a directory to output the transaction summary file.");
+			System.out.println("You must provide a valid service file and a directory to output the transaction summary file.");
 			System.exit(1);
 		}
 		
-		// Validate files here
+		// Read args
+		String vsFile = "";//args[1];
+		String transDir = "";//args[2];
 		
+		// Ensure directory exists
+		File f = new File(transDir);
+		if (!f.exists()) {
+			System.out.println("Directory does not exist");
+			//System.exit(1);
+		}
+		
+		// Ensure valid services file exists
+		File f2 = new File(vsFile);
+		if (!f2.exists()) {
+			System.out.println("Valid service file does not exist");
+			//System.exit(1);
+		}
 		
 		// Go to main case switch
 		input.runQIES();

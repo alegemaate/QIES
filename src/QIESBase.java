3c643534e4b0284bf9a0422715f4ac2fe8ec6f96
Allan Legemaate <alegemaate@gmail.com>
2018-10-13 17:21:58 -0400

    Main is void as it should be

--- src/QIESBase.java
+++ src/QIESBase.java
@@ -8,11 +8,11 @@ public class QIESBase {
 	// Input handler
 	public static InputHandler input = new InputHandler();
 	
-	public static int main(String [] args) {
+	public static void main(String [] args) {
 		// Ensure we are receiving a input file and a directory for output
 		if (args.length != 3) {
 			System.out.println("You must provide a valid input file and a directory to output the transaction summary file.");
-			return -1;
+			System.exit(1);
 		}
 		
 		// Validate files here
@@ -20,8 +20,5 @@ public class QIESBase {
 		
 		// Go to main case switch
 		input.runQIES();
-		
-		// Exit
-		return 0;
 	}
 }

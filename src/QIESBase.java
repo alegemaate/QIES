fa96db1f79058f922ec37b8514f952c48bf828a3
Irena <dunjicirena@gmail.com>
2018-10-15 13:22:33 -0400

    Updated comments

--- src/QIESBase.java
+++ src/QIESBase.java
@@ -9,36 +9,51 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 
+/*
+ * QIESBase
+ * Launch point for the program.
+ * Receives 2 arguments:
+ * 		a) Valid Services File (input file)
+ * 		b) Directory for transaction summary file output
+ */
 public class QIESBase {
 	// Input handler
 	public static InputHandler input = new InputHandler();
 	
+	/*
+	 * MAIN METHOD: starting point for program.
+	 * 
+	 * Input: Valid Services File (input file), Directory for Transaction Summary
+	 * 		  File output
+	 * Output: none
+	 */
 	public static void main(String [] args) {
 		// Ensure we are receiving a input file and a directory for output
 		if (args.length != 3 && false) {
 			System.out.println("You must provide a valid service file and a directory to output the transaction summary file.");
 			System.exit(1);
-		}
+		} // end if
 		
 		// Read args
-		String vsFile = "";//args[1];
-		String transDir = "";//args[2];
+		String vsFile = ""; //args[1];
+		String transDir = ""; //args[2];
 		
 		// Ensure directory exists
 		File f = new File(transDir);
 		if (!f.exists()) {
 			System.out.println("Directory does not exist");
 			//System.exit(1);
-		}
+		} // end if
 		
 		// Ensure valid services file exists
 		File f2 = new File(vsFile);
 		if (!f2.exists()) {
 			System.out.println("Valid service file does not exist");
 			//System.exit(1);
-		}
+		} // end if
 		
 		// Go to main case switch
 		input.runQIES();
-	}
-}
+	} // end main
+	
+} // end QIESBase class

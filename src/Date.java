de72c0bdd8c0ee494b574c2f2e8470055498261b
Irena <dunjicirena@gmail.com>
2018-10-15 14:06:09 -0400

    Date comments

--- src/Date.java
+++ src/Date.java
@@ -1,6 +1,6 @@
 /*
  * Date
- * object to hold the date information for the system
+ * Object to hold the date information for the system.
  * Spice Tests
  * 13/10/2018
  */
@@ -10,6 +10,8 @@ public class Date {
 	int year;
 	int month;
 	int day;
+	
+	//---------------------------------------------------------------------------------------------
 	
 	/*
 	 * Date constructor:
@@ -27,30 +29,50 @@ public class Date {
 		setDay(day);
 	} // end setDate method
 	
+	//---------------------------------------------------------------------------------------------
+	
 	/*
-	 * returns int year
+	 * GETYEAR: Returns year.
+	 * 
+	 * Input: none
+	 * Output: year as int
 	 */
 	public int getYear() {
 		return year;
 	} // end getYear method
 	
+	//---------------------------------------------------------------------------------------------
+	
 	/*
-	 * returns int month
+	 * GETMONTH: Returns month.
+	 * 
+	 * Input: none
+	 * Output: month as int
 	 */
 	public int getMonth() {
 		return month;
 	} // end getMonth method
 	
+	//---------------------------------------------------------------------------------------------
+	
 	/* 
-	 * returns int day
+	 * GETDAY: Returns day.
+	 * 
+	 * Input: none
+	 * Output: day as int
 	 */
 	public int getDay() {
 		return day;
 	} // end getDay method
 	
+	//---------------------------------------------------------------------------------------------
+	
 	/*
-	 * validates that the year is within range
-	 * if the year is valid (between 1980 and 2999 inclusive), sets year attribute
+	 * SETYEAR: Validates that the year is within range.
+	 * 		If the year is valid (between 1980 and 2999 inclusive), sets year attribute.
+	 * 
+	 * Input: int representing year
+	 * Output: return status (0 if valid, -1 otherwise)
 	 */
 	public int setYear(int year) {
 		if (year >= 1980 && year <= 2999) {
@@ -62,9 +84,14 @@ public class Date {
 		} // end if/else
 	} // end setYear method
 	
+	//---------------------------------------------------------------------------------------------
+	
 	/*
-	 * validates that the month is within range
-	 * if the month is valid (between 1 and 12 inclusive), sets month attribute
+	 * SETMONTH: Validates that the month is within range.
+	 * 		If the month is valid (between 1 and 12 inclusive), sets month attribute.
+	 * 
+	 * Input: int representing month
+	 * Output: return status (0 if valid, -1 otherwise)
 	 */
 	public int setMonth(int month) {
 		if (month >= 1 && month <= 12) {
@@ -76,9 +103,14 @@ public class Date {
 		} // end if/else
 	} // end setMonth method
 	
+	//---------------------------------------------------------------------------------------------
+	
 	/*
-	 * validates that the day is within range
-	 * if the day is valid (between 1 and 31 inclusive), sets day attribute
+	 * SETDAY: Validates that the day is within range.
+	 * 		If the day is valid (between 1 and 31 inclusive), sets day attribute.
+	 * 
+	 * Input: int representing day
+	 * Output: return status (0 if valid, -1 otherwise)
 	 */
 	public int setDay(int day) {
 		if (day >= 1 && day <= 31) {
@@ -90,8 +122,13 @@ public class Date {
 		} // end if/else
 	} // end setDay method
 	
+	//---------------------------------------------------------------------------------------------
+	
 	/*
-	 * returns the date as a string formatted as YYYYMMDD
+	 * TOSTRING: Returns the date as a string formatted as YYYYMMDD.
+	 * 
+	 * Input: none
+	 * Output: date as String in YYYYMMDD format
 	 */
 	@Override
 	public String toString() {

72780daf6df7f37d15bc8a0c944f9a985d414ac8
Allan Legemaate <alegemaate@gmail.com>
2018-10-30 18:36:03 -0400

    Converting Newlines to linux

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Counters
-successes=0
 fails=0
+successes=0
+tests_run=0
 
 for line in $(find . -iname 'input.txt'); do
 	# Lines from input
@@ -24,15 +25,22 @@ for line in $(find . -iname 'input.txt'); do
 	cp "../build/transactions/txnsum.txt" "${DIR}/txnsum.txt"
 	
 	# Compare files
-	if ! cmp "${DIR}/txnsum.txt" "${DIR}/output_file.txt"~ >/dev/null 2>&1
+	if cmp -s "${DIR}/txnsum.txt" "${DIR}/output_file.txt"
 	then
+		echo "SUCCESS"
+		successes=$((successes+1))
+	else
 		echo "FAILED"
 		diff "${DIR}/txnsum.txt" "${DIR}/output_file.txt"
-	else
-		echo "SUCCESS"
+		fails=$((fails+1))
 	fi
 
 	rm "${DIR}/txnsum.txt"
 	echo -e "\n\n"
+	
+	tests_run=$((tests_run+1))
 done
+
+# Output
+echo -e "TESTS COMPLETE:\n Fails:${fails}\n Successes:${successes}\n Tests Run:${tests_run}\n"
 

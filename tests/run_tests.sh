65b31d5314f66ea17e54f7e65bc913ad60ce72ef
Allan Legemaate <alegemaate@gmail.com>
2018-10-30 17:55:38 -0400

    Better test program

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Counters
+successes=0
+fails=0
+
 for line in $(find . -iname 'input.txt'); do
 	# Lines from input
 	value="$(cat $line)"
-	
 
 	# Directory 
 	DIR=$(dirname "${line}")
@@ -12,6 +15,7 @@ for line in $(find . -iname 'input.txt'); do
 	echo -e "===========================\n${DIR}\n===========================\n"
 	
 	# Run Test
+	cp "${DIR}/input_file.txt" "../build/vsf.txt"
 	cd "../build/"
 	echo -e "${value}\nexit" | java -cp ../bin "QIESBase" "vsf.txt"
 	
@@ -19,11 +23,16 @@ for line in $(find . -iname 'input.txt'); do
 	cd "../tests/"
 	cp "../build/transactions/txnsum.txt" "${DIR}/txnsum.txt"
 	
-	
 	# Compare files
-	echo -e "\n\nDIFF\n"
-	diff "${DIR}/txnsum.txt" "${DIR}/output_file.txt"
+	if ! cmp "${DIR}/txnsum.txt" "${DIR}/output_file.txt"~ >/dev/null 2>&1
+	then
+		echo "FAILED"
+		diff "${DIR}/txnsum.txt" "${DIR}/output_file.txt"
+	else
+		echo "SUCCESS"
+	fi
+
 	rm "${DIR}/txnsum.txt"
-	echo -e "\n\n\n"
+	echo -e "\n\n"
 done
 

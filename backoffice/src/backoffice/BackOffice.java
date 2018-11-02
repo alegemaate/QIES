63103478b97c7811257068432c1ff5aa96cb52fd
Allan Legemaate <alegemaate@gmail.com>
2018-11-02 18:13:26 -0400

    Plan for main file parsing

--- backoffice/src/backoffice/BackOffice.java
+++ backoffice/src/backoffice/BackOffice.java
@@ -4,6 +4,20 @@ public class BackOffice {
 
 	public static void main(String[] args) {
 		System.out.println("Backoffice!");
+		
+		// First parse CSF and load into services
+		
+		// Parse TXN SUM file
+		//  Separate lines by service number
+		//  Parse separated lines
+		//    in case of create service, create the service
+		//    sum transactions (del are negative sel are positive)
+		//    in case of reaching a delete, sum = 0; delete service
+		//    there may be a case where the service is then created
+		//    again.. if there is another line it MUST be create service
+		//    sum starts again...
+		//  Take sums for each service (if not deleted) and add to services
+		
 	}
 
 }

9628d3b8c581f4cf4e15629540bf2170dfd09216
Allan Legemaate <alegemaate@gmail.com>
2018-11-16 17:27:30 -0500

    Renamed

--- backoffice/src/CancelTicketUnitTest.java
+++ backoffice/src/CancelTicketUnitTest.java
@@ -1,5 +1,5 @@
 
-public class ChangeServiceUnitTest {
+public class CancelTicketUnitTest {
 	public static void main(String[] args) {
 		
 	}
--- backoffice/src/CreateServiceUnitTest.java
+++ backoffice/src/CreateServiceUnitTest.java
@@ -1,5 +1,12 @@
+/*
+ * CreateServiceUnitTest
+ * Harness for testing create service
+ * Spice Tests
+ * 16/11/2018
+ */
 
 public class CreateServiceUnitTest {
+	// Entry point for unit test
 	public static void main(String[] args) {
 		
 	}
